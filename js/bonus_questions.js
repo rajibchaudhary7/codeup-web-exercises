7235aa365595be725c443471a9d071f615432307
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2022-09-26 17:32:07 -0400

    HTML forms exercises

--- js/bonus_questions.js
+++ js/bonus_questions.js
@@ -96,6 +96,18 @@ function calculateTax(totalPaid, taxPercent) {
         return false;
     }
 }
-
-
 console.log(calculateTax(25, 8));
+
+// Write a function to return an array of only the Even numbers from an array with the numbers 1 through 10
+// example input: [1,2,3,4,5] expected output: [2,4]
+var myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let x = 1, x <= 5, x++) {
+    console.log(myArray[x]);
+
+}
+
+// Write a function that takes an array of objects and a string as arguments. Add a property with key ‘continent’
+// and value equal to the string to each of the objects. Return the new array of objects. Don’t mutate the original array.
+//     example input: [{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'
+// expected output: [{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand',
+//     continent: 'Asia' }]
