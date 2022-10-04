dd76e542a0328abfbe4a0ad51d420b65e6bf417a
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2022-10-03 20:26:00 -0400

    Flexbox fundamentals exercises

--- js/bonus-qs.js
+++ js/bonus-qs.js
@@ -152,3 +152,15 @@ createObject("Bangkok", "Thailand");
 //     })
 // }
 // addObject(objectsArray);
+// re-write the following using a for loop: var i = 20; while (i >= 0)
+// { console.log(i); i--; }
+for (let i = 20; i >= 0; i--) {
+    console.log(i);
+}
+// Add ‘strawberry’ to the beginning, middle, and end of the array. var
+// fruits = ["mango", "blueberries", "oranges", "banana", "papaya", "kiwi"];
+var fruits = ["mango", "blueberries", "oranges", "banana", "papaya", "kiwi"];
+fruits.unshift("strawberry");
+fruits.push("strawberry");
+fruits.splice(4, 0, "strawberry")
+console.log(fruits);
